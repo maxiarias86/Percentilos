9a7c7f9ad42e9349beb5025ebf48ffb2f3d6437a
Maxi <128001873+maxiarias86@users.noreply.github.com>
2025-08-09 10:16:54 -0300

    Commit inicial

---
+++ src/main/java/fetal/percentiles/PercentilesApplication.java
@@ -0,0 +1,13 @@
+package fetal.percentiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PercentilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PercentilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fetal/percentiles/PercentilesApplicationTests.java
@@ -0,0 +1,13 @@
+package fetal.percentiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PercentilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
